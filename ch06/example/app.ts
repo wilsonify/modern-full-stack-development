89a246c22142243e8412851c6cdbd30a27029729
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-09-05 10:05:18 -0400

    namespace example

--- ch06/example/app.ts
+++ ch06/example/app.ts
@@ -1,2 +1,9 @@
+
+/// <reference path="file1.ts" />
+/// <reference path="file2.ts" />
+import h = MyFirstNamespace.homeworld;
 SomeNS.someFunc1();
 SomeNS.someFunc2();
+
+import sf = SomeNS.InnerNS.someFunc;
+sf();
--- ch06/example/file2.ts
+++ ch06/example/file2.ts
@@ -1,1 +1,8 @@
 namespace SomeNS { export someFund2() { } }
+
+namespace SomeNS {
+  export namespace InnerNS {
+    export someFunc() { }
+  }
+}
+SomeNS.InnerNS.someFunc();
